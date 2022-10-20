892d774eb11eeb176e0d8a0a049f187cfc214ed3
JOSE HUGO DOS SANTOS SILVA <jose.hssilva4@senacsp.edu.br>
2022-10-20 11:27:55 -0300

    project actulize

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,28 +3,27 @@ import { Request, Response } from 'express'
 
 const router = Router()
 
-//criando a rota HOME
-router.get("/",(req:Request,res:Response) =>{
-    res.send("ROTA HOME FUNCIONANDO")
-})
+//importando o controller
 
+import * as pageController from '../controllers/pageController'
+
+import * as searchController from '../controllers/searchController'
+
+//criando a rota HOME
+router.get("/",pageController.home)
+ 
 
 // ROTA Cachorro
-router.get("/cachorros",(req:Request,res:Response) =>{
-    res.send("ROTA Página Cachorro")
-})
-
+router.get("/dogs",pageController.dogs)
 
 // ROTA Gato
-router.get("/gatos",(req:Request,res:Response) =>{
-    res.send("ROTA Página Gato")
-})
-
+router.get("/cats",pageController.cats)
 // Rota Peixe
 
-router.get("/peixes",(req:Request,res:Response) =>{
-    res.send("Rota Página Peixe")
-})
+router.get("/fishes",pageController.fishes)
+
+router.get("/search", searchController.search)
+
 
 //exportando o arquivo index.ts
 export default router
